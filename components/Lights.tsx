bb735b8c94dc499aa9fba3670c4e37323b854b41
mateusSleite <mateusleite2709@gmail.com>
2024-06-25 11:09:09 -0300

    bus add

---
+++ components/Lights.tsx
@@ -0,0 +1,30 @@
+import { useHelper } from "@react-three/drei";
+import { useRef } from "react";
+import { DirectionalLight, DirectionalLightHelper } from "three";
+
+const Lights: React.FC = () => {
+
+    const lightRef = useRef<THREE.DirectionalLight>();
+
+    useHelper(lightRef, DirectionalLightHelper, 5, "red");
+
+    return (
+        <>
+            <ambientLight intensity={0.2} />
+            <directionalLight
+                ref={lightRef}
+                position={[0, 10, 10]}
+                castShadow
+                shadow-mapSize-height={1000}
+                shadow-mapSize-width={1000}
+                shadow-camera-left={-20}
+                shadow-camera-right={20}
+                shadow-camera-top={20}
+                shadow-camera-bottom={-20}
+            />
+            <hemisphereLight args={["#7cdbe6", "#5e9c49", 0.7]}/>
+        </>
+    );
+}
+
+export default Lights;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,47 @@
 import type { NextPage } from 'next'
 import { Canvas } from '@react-three/fiber'
-import AnimatedBox from '../components/AnimatedBox';
-import { OrbitControls, Stats } from '@react-three/drei';
+import { OrbitControls, Stats, useTexture } from '@react-three/drei';
+import Lights from '../components/Lights';
+import Ground from '../components/Ground';
 
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const Bus = () => {
+  const model = useLoader(GLTFLoader, './models/bus.glb')
+
+  return <primitive object={model.scene}/>
+}
+
+const TexturedSpheres = () => {
+  const map = useTexture("./textures/TCom_Wall_Stone3_2x2_512_albedo.png");
+  const normalMap = useTexture("./textures/TCom_Wall_Stone3_2x2_512_normal.png");
+  const roughnessMap = useTexture("./textures/TCom_Wall_Stone3_2x2_512_roughness.png");
+
+  return (
+    <>
+        <mesh scale={[0.5, 0.5, 0.5]} position={[-0, 1, 0]} castShadow>
+          <sphereGeometry />
+          <meshStandardMaterial map={map} normalMap={normalMap} roughnessMap={roughnessMap} />
+        </mesh>
+    </>
+  );
+}
 
 const Home: NextPage = () => {
   const conVisible = true;
 
   return (
     <div className="container">
-      <Canvas>
+      <Canvas shadows>
         {conVisible ? <Stats /> : null}
         {conVisible ? <axesHelper args={[2]} /> : null}
-        {conVisible ? <gridHelper args={[10,10]}/> : null}
+        {conVisible ? <gridHelper args={[10, 10]} /> : null}
         <OrbitControls />
-        <ambientLight intensity={0.1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
-        <AnimatedBox isVisible={conVisible}/>
+        {/* <TexturedSpheres /> */}
+        <Bus />
+        <Lights />
+        <Ground />
       </Canvas>
     </div>
   );
