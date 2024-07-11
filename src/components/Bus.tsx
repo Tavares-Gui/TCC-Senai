c95ddf91b0081a8bd28e2d7452f2ba2873a61392
mateusSleite <mateusleite2709@gmail.com>
2024-07-11 10:35:49 -0300

    light ready

---
+++ src/components/Bus.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef, forwardRef, useImperativeHandle } from 'react';
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import * as THREE from "three";
+
+const Bus = forwardRef((props, ref) => {
+    const busModel = useLoader(GLTFLoader, './models/bus.glb');
+    const boundingBoxRef = useRef(new THREE.Box3());
+
+    useEffect(() => {
+        if (busModel) {
+            busModel.scene.rotation.set(0, Math.PI / 2, 0);
+            busModel.scene.position.set(2, 0, 4);
+            busModel.scene.traverse((objeto) => {
+                if (objeto instanceof THREE.Mesh) {
+                    objeto.castShadow = true;
+                }
+            });
+
+            boundingBoxRef.current.setFromObject(busModel.scene);
+        }
+    }, [busModel]);
+
+    useImperativeHandle(ref, () => ({
+        getBoundingBox: () => boundingBoxRef.current
+    }));
+
+    return (
+        <primitive object={busModel.scene} />
+    );
+});
+
+Bus.displayName = "Bus";
+
+export default Bus;
