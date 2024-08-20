e617f454d0f76670c3c957deb62291955f86cc07
mateusSleite <mateusleite2709@gmail.com>
2024-08-20 11:11:00 -0300

    tamoindomeu

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,18 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
 import CanvasGroup from "./playerHome";
 import Overlay from "./overlay";
 
 const HomePage = () => {
   return (
-    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
-      <Overlay />
-      <CanvasGroup />
-    </div>
+    <Grid container>
+      <Grid item xs={7}>
+        <Overlay />
+      </Grid>
+      <Grid item xs={5}>
+        <CanvasGroup />
+      </Grid>
+    </Grid>
   );
 };
 
--- src/pages/overlay.tsx
+++ src/pages/overlay.tsx
@@ -3,6 +3,11 @@ const Overlay = () => {
     <>
       <div className="colorTop" />
       <div className="body">
+        <h1>BEM-VINDO AO</h1>
+        <h1>BEM-VINDO AO</h1>
+
+        <h1>BEM-VINDO AO</h1>
+
 
       </div>
     </>
