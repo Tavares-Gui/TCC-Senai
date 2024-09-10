4dd84fb6ba73203a7546f711590e1776bb4917ab
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-09-10 11:05:54 -0300

    Start of the animation

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -1,28 +1,35 @@
 import React from "react";
 import Image from "next/image";
-import { div } from "three/examples/jsm/nodes/Nodes";
 import logoBosch from "../../public/images/logo_bosch.png";
-import logoFCM from "../../public/images/FCM.svg"
+import logoFCM from "../../public/images/FCM.svg";
 import logoGR from "../../public/images/GR.svg";
-import lamp from "../../public/images/Lamp.png"
+import lamp from "../../public/images/Lamp.png";
+import bus from "../../public/images/Bus.png";
 
 const LoadingPage = () => {
   return (
     <div>
       <div className="colorTop"></div>
-      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-        <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', textAlign: 'center', width: '30rem'}}>
-          <Image src={lamp} alt="Lampada de ideia" className="boschImage" style={{width: '3.6rem', height: '4rem'}}/>
-          <h2>Aqui estará escrito alguma informacao aleatória sobre o nosso sistema</h2>
-          <hr style={{width: '12em', border: '2px solid #00000'}}></hr>
+      
+      <div className="divLoading">
+        <div className="divDescLoading">
+          <Image src={lamp} alt="Lâmpada de ideia" style={{ width: '3.6rem', height: '4rem' }} />
+          <h2 style={{ fontSize: '1.25rem', fontWeight: '600', marginTop: '1rem' }}>
+            Aqui estará escrito alguma informacao aleatória sobre o nosso sistema
+          </h2>
+          <hr style={{ width: '12em', border: '1px solid black', marginTop: '1rem' }} />
+        </div>
+        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginTop: '1rem' }}>
+          <Image src={logoBosch} alt="Logo Bosch" className="logosLoading"/>
+          <h1 className="pipeLineLoading" style={{ color: '#007bc0' }}>|</h1>
+          <Image src={logoFCM} alt="Logo FCM" className="logosLoading"/>
+          <h1 className="pipeLineLoading" style={{ color: '#18837e' }}>|</h1>
+          <Image src={logoGR} alt="Logo GR" className="logosLoading"/>
         </div>
       </div>
-      <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center',}}>
-        <Image src={logoBosch} alt="Logo Bosch" className="boschImage" />
-        <h1 style={{fontSize: '1em', color: '#007bc0', marginLeft: 3, marginRight: 4, marginTop: 5}}>|</h1>
-        <Image src={logoFCM} alt="Logo FCM" className="boschImage" />
-        <h1 style={{fontSize: '1em', color: '#18837e', marginLeft: 3, marginRight: 4, marginTop: 5}}>|</h1>
-        <Image src={logoGR} alt="Logo GR" className="boschImage" />
+
+      <div className="busContainer">
+        <Image src={bus} alt="Ônibus em movimento" className="busAnimation" />
       </div>
     </div>
   );
