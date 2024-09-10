f59ae0fee791c439537513469f4e6c58ba66d825
mateusSleite <mateusleite2709@gmail.com>
2024-09-10 10:32:52 -0300

    insights add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,8 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <SpeedInsights />
-      <Analytics />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
