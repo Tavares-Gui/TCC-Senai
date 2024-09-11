e1818b95e9c73de1d8ddd465ac8f631c6eb7fc6a
Guilherme Rodrigues Tavares <guilherme.rtavares@outlook.com>
2024-09-11 15:28:00 -0300

    Loading screen almost finished

--- src/pages/loading.tsx
+++ src/pages/loading.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import logoBosch from "../../public/images/logo_bosch.png";
 import logoFCM from "../../public/images/FCM.svg";
@@ -6,7 +6,30 @@ import logoGR from "../../public/images/GR.svg";
 import lamp from "../../public/images/Lamp.png";
 import bus from "../../public/images/Bus.png";
 
+const messages = [
+  "Informação 1 sobre o sistema.",
+  "Informação 2 sobre o sistema.",
+  "Informação 3 sobre o sistema.",
+  "Informação 4 sobre o sistema.",
+  "Informação 5 sobre o sistema.",
+  "Informação 6 sobre o sistema.",
+  "Informação 7 sobre o sistema.",
+  "Informação 8 sobre o sistema.",
+  "Informação 9 sobre o sistema.",
+  "Informação 10 sobre o sistema."
+];
+
 const LoadingPage = () => {
+  const [currentMessageIndex, setCurrentMessageIndex] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentMessageIndex(prevIndex => (prevIndex + 1) % messages.length);
+    }, 10000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div>
       <div className="colorTop"></div>
@@ -15,7 +38,7 @@ const LoadingPage = () => {
         <div className="divDescLoading">
           <Image src={lamp} alt="Lâmpada de ideia" style={{ width: '3.6rem', height: '4rem' }} />
           <h2 style={{ fontSize: '1.25rem', fontWeight: '600', marginTop: '1rem' }}>
-            Aqui estará escrito alguma informacao aleatória sobre o nosso sistema
+            {messages[currentMessageIndex]}
           </h2>
           <hr className="hrLoading" />
         </div>
